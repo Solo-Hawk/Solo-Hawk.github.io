391358c063be408109360330739e5360b647571d
Solo-Hawk <daniel.fowke@adacollege.org.uk>
2017-03-22 00:58:59 +0000

    First Stage Checked

--- HCI Development v2/JavaScripts/rooms.js
+++ HCI Development v2/JavaScripts/rooms.js
@@ -1,4 +1,6 @@
 function handleResponse() {
+    back.removeEventListener('click',handleResponse)
+    
     json = rooms.responseText
     console.log(json);
     json = JSON.parse(json)
@@ -15,11 +17,15 @@ function handleResponse() {
     floors[0].addEventListener('click', show_ground_floor)
     floors[1].innerHTML = "First Floor";
     floors[1].addEventListener('click', show_first_floor)
+    back.href = "index.html"
 }
 
 var options = document.getElementById('menubuttons');
 
+var output = document.getElementById('output');
 var floors = new Array(2);
+
+var back = document.getElementById('back');
 
 var rooms = new XMLHttpRequest();
 //handleResponse();
@@ -36,7 +42,9 @@ rooms.send();
 
 
 function show_ground_floor() {
-    console.log(json)
+    back.href = "";
+    
+    back.addEventListener('click',handleResponse)
 
     var groundFloor = new Array(json['College_Rooms_Floors'][0].length);
     options.innerHTML = ""
@@ -44,9 +52,11 @@ function show_ground_floor() {
     for (var i = 0; i < groundFloor.length; i++) {
         console.log(i)
         floors[i] = document.createElement("a");
+        floors[i].id = i
         floors[i].className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent menu-button";
         floors[i].style.color = "Black";
         floors[i].innerHTML = json['College_Rooms_Floors'][0][i]['Room_Name']
+        floors[i].addEventListener('click',ground_floor_press);
         options.appendChild(floors[i]);
         options.appendChild(document.createElement("br"));
     }
@@ -54,21 +64,61 @@ function show_ground_floor() {
 }
 
 function show_first_floor() {
-console.log(json)
-
+    back.href = "";
+back.addEventListener('click',handleResponse)
     var firstFloor = new Array(json['College_Rooms_Floors'][1].length);
     options.innerHTML = ""
     console.log(json['College_Rooms_Floors'][1].length)
     for (var i = 0; i < firstFloor.length; i++) {
         console.log(i)
+        
         floors[i] = document.createElement("a");
+        floors[i].id = i
         floors[i].className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent menu-button";
         floors[i].style.color = "Black";
         floors[i].innerHTML = json['College_Rooms_Floors'][1][i]['Room_Name']
+        floors[i].addEventListener('click',first_floor_press);
         options.appendChild(floors[i]);
         options.appendChild(document.createElement("br"));
     }
 }
+function ground_floor_press() {
+    var temp
+    options.innerHTML = "";
+    output.innerHTML = json['College_Rooms_Floors'][0][parseInt(this.id)]['Room_Name']
+    output.appendChild(document.createElement("br"));
+    temp = document.createElement("section")
+    temp.innerHTML = 
+        "Room Owner - " + json['College_Rooms_Floors'][0][parseInt(this.id)]['Room_Owner'] + "<br>" + 
+        "Room Subject - " + json['College_Rooms_Floors'][0][parseInt(this.id)]['Room_Subject'] + "<br>" + "Room Size - " + json['College_Rooms_Floors'][0][parseInt(this.id)]['Room_Size'] + "<br>" + 
+         json['College_Rooms_Floors'][1][parseInt(this.id)]['Room_Info'];
+    output.appendChild(temp);
+    
+    
+    
+    
+}
+function first_floor_press() {
+    var temp
+    options.innerHTML = "";
+    output.innerHTML = json['College_Rooms_Floors'][1][parseInt(this.id)]['Room_Name']
+    output.appendChild(document.createElement("br"));
+    temp = document.createElement("section")
+    temp.innerHTML = 
+        "Room Owner - " + json['College_Rooms_Floors'][1][parseInt(this.id)]['Room_Owner'] + "<br>" + 
+        "Room Subject - " + json['College_Rooms_Floors'][1][parseInt(this.id)]['Room_Subject'] + "<br>" + "Room Size - " + json['College_Rooms_Floors'][1][parseInt(this.id)]['Room_Size'] + "<br>" + 
+         json['College_Rooms_Floors'][1][parseInt(this.id)]['Room_Info']
+    output.appendChild(temp);
+    
+    
+    
+    
+    
+}
+
+
+
+
 function runDialog(){
 var dialog = document.querySelector('dialog');
 var showDialogButton = document.querySelector('#accessibility');
